7cd74ab6062f4202503ac451780d8559a7ac7bc0
Pav0l <pavol.karas@gmail.com>
2019-03-04 14:40:50 +0100

    Create basic components

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import TodoItem from './components/TodoItem';
 
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello World!
+        <h1>Redux-list</h1>
+        <form>
+          <input
+            type="text"
+            placeholder="Enter to do item"
+          >
+          </input>
+          <button>Button</button>
+        </form>
+
+        <ul>
+          <TodoItem />
+        </ul>
       </div>
     );
   }
